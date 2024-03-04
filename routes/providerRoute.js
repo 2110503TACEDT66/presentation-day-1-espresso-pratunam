11c07f8807d7a78671ea32ce701b749fcc270e18
understann <nattapon.how@gmail.com>
2024-03-04 23:04:44 +0700

    Update providerRoute.js

--- routes/providerRoute.js
+++ routes/providerRoute.js
@@ -5,10 +5,10 @@ const {protect, authorize} = require("../middleware/authMiddleware");
 const {getProviders, getProvider} = require("../controllers/providerController");
 
 //ANCHOR getProvider and getProviders
-// getProvider : admin, user
-// getProviders : admin, user
-router.use('/:id',protect,authorize('admin', 'user'),getProvider);
-router.use('/',protect,authorize('admin', 'user'),getProviders);
+// getProvider : admin, user, provider
+// getProviders : admin, user, provider
+router.use('/:id',protect,authorize('admin', 'user','provider'),getProvider);
+router.use('/',protect,authorize('admin', 'user', 'provider'),getProviders);
 
 
 module.exports = router;
